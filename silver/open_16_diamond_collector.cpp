c1aaec239f57410f325bbca17a44904375c9d4a7
derpman23 <80171293+derpman23@users.noreply.github.com>
2022-01-06 14:03:21 -0500

    simple two pointer search and dp

---
+++ silver/open_16_diamond_collector.cpp
@@ -0,0 +1,34 @@
+#include<algorithm>
+#include<fstream>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+
+ifstream in("diamond.in");
+ofstream out("diamond.out");
+
+int n, k, d[50000], a[50000], b[50001], ans = 0;
+
+int main() {
+	in >> n >> k;
+
+	for (int i = 0; i < n; i++)
+		in >> d[i];
+
+	sort(d, d + n);
+
+	for (int l = 0, r = 0; l < n; l++) {
+		while (r < n && d[r] - d[l] <= k)
+			r++;
+		a[l] = r - l;
+	}
+
+	for (int i = n - 1; i >= 0; i--)
+		b[i] = max(b[i + 1], a[i]);
+
+	for (int i = 0; i < n; i++)
+		ans = max(ans, a[i] + b[a[i] + i]);
+
+	out << ans << "\n";
+}
