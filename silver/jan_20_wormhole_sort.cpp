4373cf30d6ca4fbe7cabfb6166b584ea5728cce0
derpman23 <80171293+derpman23@users.noreply.github.com>
2022-01-07 14:50:23 -0500

    simple binary search and dsu

--- silver/jan_20_wormhole_sort.cpp
+++ silver/jan_20_wormhole_sort.cpp
@@ -8,14 +8,13 @@ ofstream out("wormsort.out");
 
 struct DSU {
 	vector<int> v;
-	int cnt = 1;
 
 	DSU(int n) { v = vector<int>(n, -1); }
 	int get(int x) { return v[x] < 0 ? x : v[x] = get(v[x]); }
 	int same(int a, int b) { return get(a) == get(b); }
 	int unite(int x, int y) {
 		x = get(x), y = get(y);
-		if (x == y) { cnt++; return 0; }
+		if (x == y) { return 0; }
 		if (v[x] > v[y]) swap(x, y);
 		v[x] += v[y];
 		v[y] = x;
