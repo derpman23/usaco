ad72f92b55d7ab5cf7b8bafb29b8108e9aa3c6fe
derpman23 <80171293+derpman23@users.noreply.github.com>
2022-01-08 22:31:44 -0500

    simple math

---
+++ silver/dec_20_cowntagion.cpp
@@ -0,0 +1,27 @@
+#include<cmath>
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int n, cnt[100000], ans = 0;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	cin >> n;
+	
+	for (int i = 0; i < n - 1; i++) {
+		int a, b;
+		cin >> a >> b;
+		a--, b--;
+		cnt[a]++, cnt[b]++;
+	}
+
+	cnt[0]++;
+
+	for (int i = 0; i < n; i++)
+		if (!i || cnt[i] > 1)
+			ans += ceil(log2(cnt[i]));
+
+	cout << ans + n - 1 << "\n";
+}
