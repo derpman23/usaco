7cf88e862afe3632a0ef84c67cfa228f5abc1ecc
derpman23 <80171293+derpman23@users.noreply.github.com>
2022-01-16 21:51:33 -0500

    very simple dp

---
+++ gold/dec_18_teamwork.cpp
@@ -0,0 +1,29 @@
+#include<fstream>
+
+using namespace std;
+
+ifstream in("teamwork.in");
+ofstream out("teamwork.out");
+
+int n, k, s[10000], dp[10000];
+
+int main() {
+	in >> n >> k;
+
+	for (int i = 0; i < n; i++)
+		in >> s[i];
+
+	dp[0] = s[0];
+
+	for (int i = 1; i < n; i++) {
+		int best = s[i];
+		for (int j = i; j >= 0 && i - j + 1 <= k; j--) {
+			best = max(best, s[j]);
+			
+			if (!j) dp[i] = max(dp[i], (i + 1) * best);
+			else dp[i] = max(dp[i], dp[j - 1] + (i - j + 1) * best);
+		}
+	}
+
+	out << dp[n - 1];
+}
