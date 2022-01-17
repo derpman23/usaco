c5cfe1382f7d514a484131a736017312118c224b
derpman23 <80171293+derpman23@users.noreply.github.com>
2022-01-16 21:38:30 -0500

    dp on graphs

--- gold/jan_20_time_is_mooney.cpp
+++ gold/jan_20_time_is_mooney.cpp
@@ -7,7 +7,7 @@ using namespace std;
 ifstream in("time.in");
 ofstream out("time.out");
 
-int n, m, c, moonies[1000], dp[1001][1000], ans = 0;
+int n, m, c, p[1000], dp[1001][1000], ans = 0;
 
 vector<int> adj[1000];
 
@@ -15,7 +15,7 @@ int main() {
 	in >> n >> m >> c;
 
 	for (int i = 0; i < n; i++)
-		in >> moonies[i];
+		in >> p[i];
 
 	for (int i = 0; i < m; i++) {
 		int a, b; in >> a >> b;
@@ -29,7 +29,7 @@ int main() {
 		for (int j = 0; j < n; j++)
 			if (dp[i][j] != -1)
 				for (int k : adj[j])
-					dp[i + 1][k] = max(dp[i + 1][k], dp[i][j] + moonies[k]);
+					dp[i + 1][k] = max(dp[i + 1][k], dp[i][j] + p[k]);
 		ans = max(ans, dp[i][0] - c * i * i);
 	}
 
