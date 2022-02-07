25c006f6f79f59a9644ddd8012c302a5cc398b5b
derpman23 <80171293+derpman23@users.noreply.github.com>
2022-02-06 19:00:13 -0500

    counting operations until they are equal

---
+++ silver/jan_22_seaching_for_soulmates.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+typedef long long ll;
+
+int t;
+ll x, y;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	cin >> t;
+	while (t--) {
+		cin >> x >> y;
+
+		ll ans = 0x3fffffffffffff, cnt = 0;
+		while (x != y) {
+			if (x > y) {
+				if (x % 2) x++;
+				else x /= 2;
+			}
+			else {
+				ans = min(ans, cnt + y - x);
+				if (y % 2) y--;
+				else y /= 2;
+			}
+			cnt++;
+		}
+		ans = min(ans, cnt);
+
+		cout << ans << "\n";
+	}
+}
