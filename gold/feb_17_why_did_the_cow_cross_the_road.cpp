132f6506d2fe37ae96e0c2d226bfc77bcbb7866f
derpman23 <80171293+derpman23@users.noreply.github.com>
2022-03-12 23:02:11 -0500

    basic dijkstra

---
+++ gold/feb_17_why_did_the_cow_cross_the_road.cpp
@@ -0,0 +1,57 @@
+#include<cstring>
+#include<iostream>
+#include<queue>
+#include<vector>
+
+using namespace std;
+
+struct edge {
+	int a, b, w;
+
+	friend int operator>(edge e1, edge e2) {
+		return e1.w > e2.w;
+	}
+};
+
+const int dx[16] = { 0, 1, 2, 3, 0, 1, 2, -1, -2, -3, -1, -2, 1, -1, 0, 0 };
+const int dy[16] = { 3, 2, 1, 0, -3, -2, -1, 2, 1, 0, -2, -1, 0, 0, 1, -1 };
+
+int n, t, g[100][100], len[100][100];
+
+priority_queue<edge, vector<edge>, greater<edge>> q;
+
+int main() {
+	ios_base::sync_with_stdio(0); cin.tie(0);
+
+	cin >> n >> t;
+
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			cin >> g[i][j];
+
+	memset(len, 127, sizeof len);
+
+	q.push({ 0, 0, 0 });
+	while (!q.empty()) {
+		edge e = q.top();
+		q.pop();
+
+		int d = n * 2 - 2 - e.a - e.b;
+		if (d && d < 3) len[n - 1][n - 1] = min(len[n - 1][n - 1], e.w + t * d);
+
+		for (int i = 0; i < 16; i++) {
+			int x = e.a + dx[i];
+			int y = e.b + dy[i];
+			if (x < 0 || x >= n || y < 0 || y >= n) continue;
+
+			int w = e.w + g[x][y] + t * 3;
+			if (w > len[x][y]) continue;
+
+			len[x][y] = w;
+
+			q.push({ x, y, w });
+		}
+	}
+
+	cout << len[n - 1][n - 1] << "\n";
+}
