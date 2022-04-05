ea90c550b2e6936e631c36d656f11d7414818269
derpman23 <80171293+derpman23@users.noreply.github.com>
2022-04-04 21:22:46 -0400

    dsu

--- templates/disjoint_set_union.cpp
+++ templates/disjoint_set_union.cpp
@@ -6,6 +6,7 @@ struct DSU {
 	DSU(int n) { v = vector<int>(n, -1); }
 	int get(int x) { return v[x] < 0 ? x : v[x] = get(v[x]); }
 	int same(int a, int b) { return get(a) == get(b); }
+	int size(int x) { return -v[get(x)]; }
 	int unite(int x, int y) {
 		x = get(x), y = get(y);
 		if (x == y) { return 0; }
