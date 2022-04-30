45b5664798d551cc13fb40f6ffa50daf736cec7e
derpman23 <80171293+derpman23@users.noreply.github.com>
2022-04-29 21:00:46 -0400

    straightforward dp

--- gold/feb_17_why_did_the_cow_cross_the_road_ii.cpp
+++ gold/feb_17_why_did_the_cow_cross_the_road_ii.cpp
@@ -18,9 +18,9 @@ int main() {
 
 	dp[0][0] = abs(a[0] - b[0]) <= 4;
 	for (int i = 1; i < n; i++)
-    dp[i][0] = dp[i - 1][0] || abs(a[i] - b[0]) <= 4;
+    		dp[i][0] = dp[i - 1][0] || abs(a[i] - b[0]) <= 4;
 	for (int i = 1; i < n; i++)
-    dp[0][i] = dp[0][i - 1] || abs(a[0] - b[i]) <= 4;
+    		dp[0][i] = dp[0][i - 1] || abs(a[0] - b[i]) <= 4;
 	for (int i = 1; i < n; i++)
 		for (int j = 1; j < n; j++)
 			dp[i][j] = max({ dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1] + (abs(a[i] - b[j]) <= 4) });
